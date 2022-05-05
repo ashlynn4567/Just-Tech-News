8837e1dc259928479a8df6cebb79c25d5f180249
Ashley Smith <ashlynn4567@gmail.com>
2022-05-05 16:30:58 -0500

    created association between post and user tables

--- models/index.js
+++ models/index.js
@@ -1,6 +1,17 @@
 const User = require("./User");
 const Post = require("./Post");
 
+// create associations 
+// creates association to post within user
+User.hasMany(Post, {
+    foreignKey: "user_id"
+});
+
+// creates association to user within post
+Post.belongsTo(User, {
+    foreignKey: "user_id"
+});
+
 module.exports = { 
     User,
     Post
