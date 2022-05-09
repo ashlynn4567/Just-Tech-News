957a36e54b35c9f87b2ba3bdae477981fa359d31
Ashley Smith <ashlynn4567@gmail.com>
2022-05-09 15:18:56 -0500

    added vote count capabilities as well as a vote model with associations to the user and post

--- models/index.js
+++ models/index.js
@@ -1,18 +1,50 @@
 const User = require("./User");
 const Post = require("./Post");
+const Vote = require("./Vote");
 
-// create associations 
-// creates association to post within user
+// create associations between models with their foreign keys
+// creates association to Post within User (one to many)
 User.hasMany(Post, {
     foreignKey: "user_id"
 });
 
-// creates association to user within post
+// creates association to User within Post (one to one)
 Post.belongsTo(User, {
     foreignKey: "user_id"
 });
 
+// creates association to Post through User (many to many)
+User.belongsToMany(Post, {
+    through: Vote,
+    as: "voted_posts",
+    foreignKey: "user_id"
+});
+
+// creates association to User through Post (many to many)
+Post.belongsToMany(User, {
+    through: Vote,
+    as: "voted_posts",
+    foreignKey: "post_id"
+});
+
+Vote.belongsTo(User, {
+    foreignKey: "user_id"
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: "post_id"
+});
+
+User.hasMany(Vote, {
+    foreignKey: "user_id"
+});
+
+Post.hasMany(Vote, {
+    foreignKey: "post_id"
+});
+
 module.exports = { 
     User,
-    Post
+    Post,
+    Vote
 };
